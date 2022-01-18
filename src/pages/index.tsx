8b03c3d39d4a0c4dfc2ff2bb3d584068e1db80b2
price tula <pricetula@gmail.com>
2022-01-18 20:59:49 +0300

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-function Home() { return <div>Hello</div>; }
+function Home() {
+  return (
+    <div>
+      <img
+        src="/logo.svg"
+        alt="of the author"
+      />
+    </div>
+  );
+}
 
 export default Home;
