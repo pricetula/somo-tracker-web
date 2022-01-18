5429a5f6e46c6fe0f0f45256819aad6126078dc0
price tula <pricetula@gmail.com>
2022-01-18 18:48:02 +0300

    added pages component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable */
+import React from 'react';
+import Head from 'next/head';
+
+interface AppProps {
+  Component: any
+  pageProps: any
+}
+
+class App extends React.Component<AppProps> {
+  render() {
+    const {
+      Component,
+      pageProps,
+    } = this.props;
+    return (
+      <>
+        <Head>
+          <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+            rel="stylesheet"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+function Home() { return <div>Hello</div>; }
+
+export default Home;
