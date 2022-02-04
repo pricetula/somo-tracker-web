eca693923bbd01e7bb36fa9abd891630518c08c8
price tula <pricetula@gmail.com>
2022-02-04 17:19:31 +0300

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,43 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <img
-        src="/logo.svg"
-        alt="of the author"
-      />
-    </div>
+    <section className="demo">
+      <nav className="nav">
+        <span className="nav__logo">StudentTracker</span>
+        <section className="nav__link-wrapper">
+          <ul>
+            <li>How it works</li>
+            <li>Pricing</li>
+          </ul>
+
+          <ul className="profile-links">
+            <li>signin</li>
+            <li>signup</li>
+          </ul>
+        </section>
+      </nav>
+      <div className="banner">
+        <div className="description-wrapper">
+          <h1>Short Title</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, obcaecati.</p>
+        </div>
+        <div className="image-wrapper">
+          <div className="image">image</div>
+        </div>
+      </div>
+      <div className="primary">
+        Primary
+      </div>
+      <div className="secondary">
+        Secondary
+      </div>
+      <div className="error">
+        Error
+      </div>
+      <div className="warning">
+        Warning
+      </div>
+    </section>
   );
 }
 
