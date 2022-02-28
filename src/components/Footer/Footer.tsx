f96209a3733903adddaf1af03ceefb7e20f5161a
price tula <pricetula@gmail.com>
2022-02-28 20:13:03 +0300

    added style for footer main and nav components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Footer.module.scss';
 
 function Footer() {
   return (
-    <footer>
+    <footer className={style.footer}>
       footer
     </footer>
   );
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import React, { ReactNode } from 'react';
 import TopNav from '../TopNav';
 import Footer from '../Footer';
+import style from './Layout.module.scss';
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <TopNav />
-      <main>
+      <main className={style.main}>
         {children}
       </main>
       <Footer />
