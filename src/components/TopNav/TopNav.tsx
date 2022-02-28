5282c2d38a0fb0ab2d825b346b277dd8d8d5c741
price tula <pricetula@gmail.com>
2022-02-28 23:48:10 +0300

    added side menu context method to open side menu component

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { SideMenuOpenContext } from 'src/context/sideMenuOpen';
 import Button from '../Button';
 import Logo from '../Logo';
 import style from './TopNav.module.scss';
 
 function TopNav() {
+  const sideMenu = React.useContext(SideMenuOpenContext);
   return (
     <nav className={style.nav} aria-label="Primary">
       <section className={style.logoSidebarbuttonWrapper}>
-        <Button customClass={style.sidebarbutton} onClick={() => console.log("yoooooo")}>
+        <Button
+          customClass={style.sidebarbutton}
+          onClick={() => sideMenu.setIsOpen(true)}
+        >
           <i className="material-icons">menu</i>
         </Button>
         <Logo />
