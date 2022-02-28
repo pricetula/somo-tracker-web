555a437dd5e8b947d1a49367f00364654b711e25
price tula <pricetula@gmail.com>
2022-02-28 19:28:16 +0300

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,41 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <section className="demo">
-      <nav className="nav">
-        <span className="nav__logo">StudentTracker</span>
-        <section className="nav__link-wrapper">
-          <ul>
-            <li>How it works</li>
-            <li>Pricing</li>
-          </ul>
-
-          <ul className="profile-links">
-            <li>signin</li>
-            <li>signup</li>
-          </ul>
-        </section>
-      </nav>
-      <div className="banner">
-        <div className="description-wrapper">
-          <h1>Short Title</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, obcaecati.</p>
-        </div>
-        <div className="image-wrapper">
-          <div className="image" />
-        </div>
-      </div>
-      <div className="primary">
-        Primary
-      </div>
-      <div className="secondary">
-        Secondary
-      </div>
-      <div className="error">
-        Error
-      </div>
-      <div className="warning">
-        Warning
-      </div>
+      home
     </section>
   );
 }
