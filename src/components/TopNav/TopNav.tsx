f6c1a3f1a6390f18962336454a01af738855bb15
price tula <pricetula@gmail.com>
2022-02-28 19:47:15 +0300

    clean up

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Logo from '../Logo';
+import style from './TopNav.module.scss';
 
 function TopNav() {
   return (
-    <nav aria-label="Primary">
+    <nav className={style.nav} aria-label="Primary">
       <Logo />
     </nav>
   )
