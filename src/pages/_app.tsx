6234f90ad0b620a50c15aa7cebcf905c7d5b8444
price tula <pricetula@gmail.com>
2022-03-01 05:32:40 +0300

    added script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 import Layout from 'src/components/Layout';
 import 'src/style/global/index.scss';
 
@@ -44,6 +45,14 @@ class App extends React.Component<AppProps> {
             rel="stylesheet"
           />
           <title>Ultimate student perfomance tracking system</title>
+          <Script>
+            {`
+              !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="h1mNT2zpsAk68581o1fnDZvChrezGqoV";;analytics.SNIPPET_VERSION="4.15.3";
+              analytics.load("h1mNT2zpsAk68581o1fnDZvChrezGqoV");
+              analytics.page();
+              }}();
+            `}
+          </Script>
         </Head>
         <Layout>
           <Component {...pageProps} />
