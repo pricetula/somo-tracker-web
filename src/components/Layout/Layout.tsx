77f7e03fe07bc2faa23451b9b739454ccf86db58
price tula <pricetula@gmail.com>
2022-03-02 19:13:55 +0300

    changes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,23 @@
 import React, { ReactNode } from 'react';
 import { DialogOpenContextProvider } from 'src/context/dialogOpen';
+import { SideMenuOpenContextProvider } from 'src/context/sideMenuOpen';
 import TopNav from '../TopNav';
 import Footer from '../Footer';
-import Dialog from '../Dialog';
+import Sidemenu from '../Sidemenu';
 import style from './Layout.module.scss';
 
 function Layout({ children }: { children: ReactNode }) {
   return (
-    <DialogOpenContextProvider>
-      <TopNav />
-      <Dialog />
-      <main className={style.main}>
-        {children}
-      </main>
-      <Footer />
-    </DialogOpenContextProvider>
+    <SideMenuOpenContextProvider>
+      <DialogOpenContextProvider>
+        <TopNav />
+        <Sidemenu />
+        <main className={style.main}>
+          {children}
+        </main>
+        <Footer />
+      </DialogOpenContextProvider>
+    </SideMenuOpenContextProvider>
   )
 };
 
---
+++ src/components/Sidemenu/Sidemenu.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Dialog from '../Dialog';
+import style from './Sidemenu.module.scss';
+
+const Sidemenu = () => <Dialog />;
+
+export default Sidemenu;
