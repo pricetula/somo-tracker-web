f708bdd2c987eaf3cb757dbb424652a308c9f2c0
price tula <pricetula@gmail.com>
2022-03-05 05:32:54 +0300

    changes

--- src/components/common/BreadcrumbNav/BreadcrumbNav.tsx
+++ src/components/common/BreadcrumbNav/BreadcrumbNav.tsx
@@ -1,15 +1,103 @@
 import React from 'react';
-import Popper from '../Popper';
-import sytle from './BreadcrumbNav.module.scss';
+import style from './BreadcrumbNav.module.scss';
 
-const BreadcrumbNav = () => (
-  <nav>
-    <ul>
-      <li>Bread crumb</li>
-    </ul>
-    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-    <Popper />
-  </nav>
-);
+const list = [
+  'Home',
+  'Dil emma',
+  'Por tiuncle',
+  'Rai nmaker',
+  // 'Cou rante',
+  // 'Gon goozle',
+  // 'Rio meter',
+  // 'Bla sienWonky',
+  // 'Arr ogate',
+  // 'Cre nelAgita',
+  // 'Qui nquadrate',
+  // 'Den ouement',
+  // 'Glu miferous',
+  // 'Zoc treeEgads',
+  // 'Sta lactiform',
+  // 'Rin dfleis',
+  // 'Pin khotBlewit',
+  // 'Fil ibuster',
+  // 'Cli thridiate',
+  // 'Bet hezSmegma',
+  // 'Hyd ropathy',
+  // 'Sco fflaw',
+  // 'Ism atic',
+  // 'Kid dowJest',
+  // 'Nut arian',
+  // 'Non plussed',
+  // 'Hic klanPaseo',
+  // 'Jod ikKazoo',
+  // 'Gho kaaMercer',
+  // 'Bra nnigan',
+  // 'Sed igitated',
+  // 'Arc hietGerund',
+  // 'Tri stypooStang',
+  // 'Fla bbergast',
+  // 'Diz zyduckVolta',
+];
+
+const BreadcrumbNav = () => {
+  const [shownIndexes, setShownIndexes] = React.useState([]);
+  const [hidenIndexes, setHidenIndexes] = React.useState([]);
+  const listEl = React.useRef(null);
+  const navEl = React.useRef(null);
+
+  React.useEffect(() => {
+    if (listEl && navEl) {
+      let navWidth = navEl?.current?.clientWidth || 0;
+      if (navWidth) {
+        const lItems = listEl?.current?.getElementsByTagName('li');
+        let iWidths = lItems[0]?.clientWidth || 0;
+        const shownIs = [0];
+        const hidenIs = [];
+
+        for (let i = lItems?.length || 0; i > 1; i--) {
+          const index = i-1;
+          iWidths += lItems[index]?.clientWidth;
+          console.log(iWidths, navWidth, iWidths < navWidth, list[index])
+          if (iWidths < navWidth) {
+            shownIs.push(index);
+          } else {
+            hidenIs.push(index);
+          }
+        }
+
+        setShownIndexes(shownIs);
+        setHidenIndexes(hidenIs);
+      }
+    }
+  }, [setShownIndexes, setHidenIndexes]);
+
+  const shownItems = list.filter((a, i) => shownIndexes.includes(i));
+  const hiddenItems = list.filter((a, i) => hidenIndexes.includes(i));
+
+  return (
+    <nav
+      ref={navEl}
+      aria-label="bread crumb"
+      className={style.nav}
+    >
+      <ul>
+        {shownItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {hiddenItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+
+      <ul ref={listEl} className={style.hiddenList}>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
 
 export default BreadcrumbNav;
--- src/components/common/Popper/Popper.tsx
+++ src/components/common/Popper/Popper.tsx
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
-import { usePopper } from 'react-popper';
-// import style from './Popper.module.scss';
+import React from 'react';
+import {useFloating, shift} from '@floating-ui/react-dom';
 
 const Popper = () => {
-  const [referenceElement, setReferenceElement] = useState(null);
-  const [popperElement, setPopperElement] = useState(null);
-  // const [arrowElement, setArrowElement] = useState(null);
-  const { styles, attributes } = usePopper(referenceElement, popperElement);
-
+  const {x, y, reference, floating, strategy} = useFloating({
+    placement: 'top',
+    middleware: [shift()],
+  });
+console.log(reference)
   return (
     <>
-      <button ref={setReferenceElement}>
-        Reference element
-      </button>
-
-      <span ref={setPopperElement} style={styles.popper} {...attributes.popper}>
-        Popper element
-        {/* <span ref={setArrowElement} style={styles.arrow} /> */}
-      </span>
+      <button ref={reference}>Button</button>
+      <div
+        ref={floating}
+        style={{
+          position: strategy,
+          top: y ?? '',
+          left: x ?? '',
+        }}
+      >
+        Tooltip
+      </div>
     </>
   );
-};
+}
 
 export default Popper;
