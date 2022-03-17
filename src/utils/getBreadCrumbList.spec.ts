421c3f587b903ae23b9df64c47e4b99667025a10
Price Tula <pricetula@gmail.com>
2022-03-17 08:14:48 +0300

    added function to create bread crumbs

---
+++ src/utils/getBreadCrumbList.spec.ts
@@ -0,0 +1,62 @@
+import {appRouteMap} from './routes';
+import getBreadCrumbList from './getBreadCrumbList'
+
+describe('getUrlWithInjectedValues', () => {
+    test('url with empty string to return null', () => {
+        expect(
+            getBreadCrumbList({
+                appRouteMap,
+                startingUrlPattern: ''
+            })
+        ).toEqual(null)
+    })
+
+    test('url with empty string to return null if starting url pattern not found', () => {
+        expect(
+            getBreadCrumbList({
+                appRouteMap,
+                startingUrlPattern: 'RandomFakeUrlPattterrrrnnnn'
+            })
+        ).toEqual(null)
+    })
+
+    test('url without parent href should return null', () => {
+        const list = getBreadCrumbList({
+            appRouteMap,
+            startingUrlPattern: '/tests/attempts/[id]/analytics/[analyticsid]',
+            values: {
+                id: 23,
+                analyticsid: 81,
+            }
+        })
+
+        const expectedList = [
+            {
+                label: 'home',
+                href: '/',
+              },
+              {
+                label: 'tests',
+                href: '/tests',
+              },
+              {
+                label: 'test attempts',
+                href: '/tests/attempts',
+              },
+              {
+                label: 'test attempt',
+                href: '/tests/attempts/23',
+              },
+              {
+                label: 'attempt analytics',
+                href: '/tests/attempts/23/analytics',
+              },
+              {
+                label: 'attempt analytic',
+                href: '/tests/attempts/23/analytics/81',
+              }
+        ]
+
+        expect(list.length).toEqual(expectedList.length)
+    })
+})
---
+++ src/utils/getBreadCrumbList.ts
@@ -0,0 +1,47 @@
+import getUrlWithInjectedValues, { ValuesToInjectInUrl } from './getUrlWithInjectedValues'
+import { AppRouteMap } from './routes'
+
+export interface BreadCrumb {
+    label: string,
+    href: string,
+}
+
+export type BreadCrumbs = BreadCrumb[]
+
+export interface GetBreadCrumbListInput {
+    appRouteMap: AppRouteMap,
+    startingUrlPattern: string,
+    values?: ValuesToInjectInUrl,
+}
+
+export default ({
+    values,
+    appRouteMap,
+    startingUrlPattern,
+}: GetBreadCrumbListInput): BreadCrumbs => {
+    if (
+        !startingUrlPattern ||
+        !appRouteMap[startingUrlPattern] ||
+        !appRouteMap[startingUrlPattern].parentHref
+    ) return null
+
+    const l = []
+
+    let k = startingUrlPattern
+
+    while (appRouteMap[k].href) {
+        l.push({
+            label: appRouteMap[k].label,
+            href: getUrlWithInjectedValues({
+                values,
+                urlPattern: appRouteMap[k].href
+            }),
+        })
+
+        if (!appRouteMap[k].parentHref) break
+        
+        k = appRouteMap[k].parentHref
+    }
+
+    return l
+}
