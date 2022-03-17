cc5b67b3783cc9853d3c7847a739eee3f6b7f218
Price Tula <pricetula@gmail.com>
2022-03-17 08:13:50 +0300

    added route mapping

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -6,10 +6,45 @@ export interface AppRoute {
 
 export type AppRoutes = AppRoute[];
 
+export type AppRouteMap = {[k: string]: AppRoute}
+
 export const appRouteList: AppRoutes = [
   {
     label: 'home',
     href: '/',
     parentHref: null
+  },
+  {
+    label: 'tests',
+    href: '/tests',
+    parentHref: '/'
+  },
+  {
+    label: 'test attempts',
+    href: '/tests/attempts',
+    parentHref: '/tests'
+  },
+  {
+    label: 'test attempt',
+    href: '/tests/attempts/[id]',
+    parentHref: '/tests/attempts'
+  },
+  {
+    label: 'attempt analytics',
+    href: '/tests/attempts/[id]/analytics',
+    parentHref: '/tests/attempts/[id]'
+  },
+  {
+    label: 'attempt analytic',
+    href: '/tests/attempts/[id]/analytics/[analyticsid]',
+    parentHref: '/tests/attempts/[id]/analytics'
   }
 ];
+
+export const appRouteMap = appRouteList.reduce(
+  (a, r) => ({
+    ...a,
+    [r.href]: r
+  }),
+  {},
+)
