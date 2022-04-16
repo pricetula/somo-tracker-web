8f6f6236c167b1c99877ae0b26b266d721235a60
Price Tula <pricetula@gmail.com>
2022-04-16 10:32:35 +0300

    user profile name

--- src/components/common/TopNav/TopNav.tsx
+++ src/components/common/TopNav/TopNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0';
 import { DialogOpenContext } from 'src/context/dialogOpen';
 import Button from '../Button';
 import Logo from '../Logo';
@@ -6,6 +7,12 @@ import style from './TopNav.module.scss';
 
 function TopNav() {
   const dialog = React.useContext(DialogOpenContext);
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  
+  if (error) return <div>{error.message}</div>;
+
   return (
     <nav className={style.nav} aria-label="Primary">
       <section className={style.logoSidebarbuttonWrapper}>
@@ -17,6 +24,7 @@ function TopNav() {
         <Logo />
       </section>
       <section className={style.linksWrapper}>
+        {user ? user.name : (<a href="/api/auth/login">Login</a>)}
         <Button customClass={style.authMenubutton}>
           <i className="material-icons">more_vert</i>
         </Button>
