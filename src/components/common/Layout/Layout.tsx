df4b3ef6cf3a297ec469eae83ef47919df26b638
Price Tula <pricetula@gmail.com>
2022-05-20 00:13:59 +0300

    clean up

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -1,24 +1,21 @@
 import React, { ReactNode } from 'react';
-import { DialogOpenContextProvider } from 'src/context/dialogOpen';
 import TopNav from '../TopNav';
 import Footer from '../Footer';
 import Sidemenu from '../Sidemenu';
-import Links from '../Sidemenu/components/Links';
 import style from './Layout.module.scss';
 
 function Layout({ children }: { children: ReactNode }) {
   return (
-    <DialogOpenContextProvider>
+    <>
       <TopNav />
-      <Sidemenu />
       <main className={style.main}>
-        <Links />
+        <Sidemenu />
         <section className={style.content}>
           {children}
         </section>
       </main>
       <Footer />
-    </DialogOpenContextProvider>
+    </>
   )
 };
 
--- src/components/common/Sidemenu/Sidemenu.tsx
+++ src/components/common/Sidemenu/Sidemenu.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import { DialogOpenContext } from 'src/context/dialogOpen';
 import style from './Sidemenu.module.scss';
 
 const Sidemenu = () => {
-  const dialog = React.useContext(DialogOpenContext);
-  const closeSidemenu = () => {
-    dialog.setIsSideMenuOpen(false);
-  }
   return (
     <div>sss</div>
   );
