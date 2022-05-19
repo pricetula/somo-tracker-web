6be8e0e93f42fa6d38964cb96df50fb1608904b7
Price Tula <pricetula@gmail.com>
2022-05-19 20:09:19 +0300

    added simple dialog component example

--- src/components/page/Home/Home.tsx
+++ src/components/page/Home/Home.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import Pagewrapper from 'src/components/common/Pagewrapper';
+import Dialog from 'src/components/common/Dialog'
 import style from './Home.module.scss';
 
-const Home = () => (
-  <Pagewrapper>
-    <div className={style.home}>
-      Home2
-    </div>
-  </Pagewrapper>
-);
+const Home = () => {
+  const [open, setOpen] = React.useState(false)
+
+  return (
+    <Pagewrapper>
+      <div className={style.home}>
+        <Dialog
+          open={open}
+          closeOnBackdropClick
+          onClose={() => { setOpen(false) }}
+        >
+          Example dialog
+        </Dialog>
+        <button onClick={() => setOpen(true)}>
+          Open/close
+        </button>
+      </div>
+    </Pagewrapper>
+  );
+}
 
 export default Home;
