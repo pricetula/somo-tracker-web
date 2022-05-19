5a3807c2ff55706989fc29f0bb59294b8fb45b41
Price Tula <pricetula@gmail.com>
2022-05-20 00:17:36 +0300

    removed unused style and context

--- src/components/common/TopNav/TopNav.tsx
+++ src/components/common/TopNav/TopNav.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { useUser } from '@auth0/nextjs-auth0';
-import { DialogOpenContext } from 'src/context/dialogOpen';
 import Button from '../Button';
 import Logo from '../Logo';
 import style from './TopNav.module.scss';
 
 function TopNav() {
-  const dialog = React.useContext(DialogOpenContext);
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
@@ -15,14 +13,7 @@ function TopNav() {
 
   return (
     <nav className={style.nav} aria-label="Primary">
-      <section className={style.logoSidebarbuttonWrapper}>
-        <Button
-          customClass={style.sidebarbutton}
-          leftIconName={dialog.isSideMenuOpen ? 'close' : 'menu'}
-          onClick={() => dialog.setIsSideMenuOpen(!dialog.isSideMenuOpen)}
-        />
-        <Logo />
-      </section>
+      <Logo />
       <section className={style.linksWrapper}>
         {user ? user.name : (<a href="/api/auth/login">Login</a>)}
         <Button customClass={style.authMenubutton}>
